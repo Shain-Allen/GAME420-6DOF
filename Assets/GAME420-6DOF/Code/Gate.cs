cd3465ff4600f474a5f3305b661f23b2b110a3a4
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2025-01-27 22:51:25 -0800

    Some things got left behind

---
+++ Assets/GAME420-6DOF/Code/Gate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent(out ShipController player))
+            return;
+            
+        LapManager.Instance.OnGateCrossed?.Invoke(gameObject);
+    }
+}
